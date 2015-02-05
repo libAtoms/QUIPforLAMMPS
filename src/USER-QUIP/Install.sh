f70f6c7599b9d1043e6c88a556331c57d2a13ed5
Albert Bartok-Partay <apbartok@gmail.com>
2015-02-05 12:10:39 +0000

    no need for quip_SYSINC

--- src/USER-QUIP/Install.sh
+++ src/USER-QUIP/Install.sh
@@ -7,8 +7,6 @@ if (test $1 = 1) then
     sed -i -e 's/[^ \t]*quip[^ \t]* //' ../Makefile.package
     sed -i -e 's|^PKG_INC =[ \t]*|&-I../../lib/quip |' ../Makefile.package
     sed -i -e 's|^PKG_PATH =[ \t]*|&-L../../lib/quip |' ../Makefile.package
-#    sed -i -e 's|^PKG_LIB =[ \t]*|&-lquip |' ../Makefile.package
-    sed -i -e 's|^PKG_SYSINC =[ \t]*|&$(quip_SYSINC) |' ../Makefile.package
     sed -i -e 's|^PKG_SYSLIB =[ \t]*|&$(quip_SYSLIB) |' ../Makefile.package
     sed -i -e 's|^PKG_SYSPATH =[ \t]*|&$(quip_SYSPATH) |' ../Makefile.package
   fi
